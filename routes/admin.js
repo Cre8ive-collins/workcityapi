171df69f745c1fe6a998d233d89b88876670042c
Cre8ive-collins <wilsoncollins44@yahoo.com>
2021-07-03 10:09:34 +0100

    testing endpoints

---
+++ routes/admin.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router();
+
+
+function admin(){
+    
+    router.get('/', (req, res) => {
+        res.status(200).json({
+            'message': 'Get /orders'
+        })
+    })
+
+    router.post('/login', (req, res) => {
+        const data = req.body
+        const request = req
+        res.status(201).json({
+            formBody: data,
+            requestObject: request
+        })
+    })
+
+    router.get('/:ordrId', (req, res) => {
+        res.status(200).json({
+            'message': 'Get /orders/one'
+        })
+    })
+
+    router.post('/orderId', (req, res) => {
+        res.status(201).json({
+            'message': 'post /orders/one'
+        })
+    })
+
+    return router
+}
+
+module.exports = admin()
